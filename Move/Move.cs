0439f2babdd19a1d75935109453c1b8708ddc211
igorfel <igor_colgate@hotmail.com>
2015-12-06 05:25:09 -0300

    Classe de movimentação por slide
    Pega movimentação do usuário horizontal e/ou vertical e aplica essa
    movimentação a um objeto

---
+++ Move/Move.cs
@@ -0,0 +1,64 @@
+﻿///<summary>
+/// Este script recebe um valor qualquer que representará a direção em que o objeto se movimentará, 
+/// sendo, 1 - cima, 2 - direita, 3 - baixo, 4 - esquerda. Existirão duas formas de uso para essa classe, 
+/// sendo a primeira por intervenção externa: Quando aplicada a um objeto que necessita que uma outra classe
+/// controle o movimento do objeto.
+/// A segunda por geração automática e aleatória: Quando há necessidade que o objeto se mova aleatóriamente 
+/// de tempos em tempos.
+/// </summary>
+using UnityEngine;
+using System.Collections;
+
+public class Move : MonoBehaviour {
+
+    public bool isRandom;           //É movimento gerado automáticamente ou controlado por uma classe externa
+    public float stepSize = 1;          //Quanto o objeto deve se mover
+
+	// Use this for initialization
+	void Start () {
+        if(isRandom)
+            InvokeRepeating("randomMove", 1, 3);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (!isRandom && IsInvoking("randomMove"))
+            CancelInvoke("randomMove");
+	}
+
+    //INIT METODS
+    void randomMove ()
+    {
+        int direction = Random.Range(1,5);
+
+        moveByInput(direction, transform.position);
+        //print(direction + " - " + transform.position);
+    }
+
+    public void moveByInput(int direction, Vector3 initPos)
+    {
+        float dist;
+        //Vector3 initPos = transform.position;
+
+        switch (direction)
+        {
+            case 1:
+                dist = Vector3.Distance(transform.position, new Vector3(initPos.x, initPos.y + stepSize, initPos.z));
+                transform.Translate(new Vector3(0, dist * Time.deltaTime, 0));
+                break;
+            case 2:
+                dist = Vector3.Distance(transform.position, new Vector3(initPos.x + stepSize, initPos.y, initPos.z));
+                transform.Translate(new Vector3(dist * Time.deltaTime, 0, 0));
+                break;
+            case 3:
+                dist = Vector3.Distance(transform.position, new Vector3(initPos.x, initPos.y - stepSize, initPos.z));
+                transform.Translate(new Vector3(0, -dist * Time.deltaTime, 0));
+                break;
+            case 4:
+                dist = Vector3.Distance(transform.position, new Vector3(initPos.x - stepSize, initPos.y, initPos.z));
+                transform.Translate(new Vector3(-dist * Time.deltaTime, 0, 0));
+                break;
+        }
+    }
+    //END METODS
+}
---
+++ User Inputs/UserInputs.cs
@@ -0,0 +1,68 @@
+﻿///<summary>
+/// Classe que identifica as interações do usuário, somente para mobile. Essa classe deve 
+/// identificar as ações do usuário na tela e gerar um valor a partir do toque ou movimento.
+/// Toda vez que um valor for gerado essa classe se comunica com a 'Move' gerando a movimentação do player.
+/// Ex.: Quando o usuário mover o dedo de cima para baixo o valor gerado deve ser igual a 3. 
+/// Ou quando ele mover da esquerda para a direita, o valor gerado deve ser 2.
+/// </summary>
+using UnityEngine;
+using UnityEngine.Assertions;
+using System.Collections;
+
+public class UserInputs : MonoBehaviour {
+
+    public float sensibility = .3f;       //Sensibilidade para identificação do movimento
+    public int generatedValue, oldValue;      //Valor que será enviado pra classe responsável pelo movimento
+    public Move m;
+
+
+    //INIT PROPERTIES
+    public int GeneratedValue
+    {
+        get { return generatedValue; }
+    }
+    //END PROPERTIES
+
+	// Use this for initialization
+	void Start () {
+
+        //Checa se a variavel m foi atribuida-------------
+        Assert.IsTrue(GetComponent<Move>(), "Impossível atribuir valor a variavel m, classe Move não encontrada neste objeto.");
+
+        if (!m)
+        {
+            m = GetComponent<Move>();
+        }
+        //------------------------------------------------
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if(Input.GetMouseButton(0))
+            generatedValue = input2Direction();
+
+        if (generatedValue != oldValue)
+            m.moveByInput(generatedValue, transform.position);
+    }
+
+    /// <summary>
+    /// Compara o movimento do mouse com a sensibilidade desejada se for maior o valor respectivo 
+    /// para a direção é gerado
+    /// </summary>
+    /// <returns></returns>
+    int input2Direction ()
+    {
+        if (Input.GetAxis("Mouse X") > sensibility)
+            return 2;
+        else if (Input.GetAxis("Mouse X") < -sensibility)
+            return 4;
+        else if (Input.GetAxis("Mouse Y") > sensibility)
+            return 1;
+        else if (Input.GetAxis("Mouse Y") < -sensibility)
+            return 3;
+        else
+            return 0;
+
+        oldValue = generatedValue;
+    }
+}
