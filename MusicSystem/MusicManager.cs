5cc4d2527dcac800e219d2cce92d83c9d6d9a28b
Hiran <gondim.game@gmail.com>
2015-12-29 08:59:40 -0200

    Paco de controle de áudio
    Scripts responsáveis por gerenciar o volume e a reprodução de áudio nos
    aplicativos da Loop Soft

---
+++ MusicSystem/MusicManager.cs
@@ -0,0 +1,147 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections.Generic;
+/// <summary>
+/// Apenas gerencia as alterações e envia para SoundSystem as atualizações da parte de audio.
+/// 
+/// OBS.: precisa de ajustes para caso o programa apenas tenha efeitos sonoros ou apenas musicas.
+/// </summary>
+public class MusicManager : MonoBehaviour
+{
+
+    SoundSystem test = SoundSystem.Instance;
+
+    // INIT DECLARAÇÕES DE AUDIO.
+    public List<AudioSource> musica;
+    public List<AudioSource> efeitos;
+
+    public Scrollbar volumeMusica;
+    public Scrollbar volumeEfeitos;
+    // END DECLARAÇÕES DE AUDIO.
+
+    // INIT METODOS DA CLASSE
+    public static bool flag_OnOffMusic = true;
+    public static bool flag_OnOffEffects = true;
+    public static bool flag_OnOffMusicEffects = true;
+    void InitConf()
+    {
+        if (flag_OnOffMusic)
+            test.VolumeControl(musica, 1);
+
+        else
+            test.VolumeControl(musica, 0);
+
+        if (flag_OnOffEffects)
+            test.VolumeControl(efeitos, 1);
+
+        else
+            test.VolumeControl(efeitos, 0);
+
+        if (flag_OnOffMusicEffects)
+        {
+            test.VolumeControl(musica, 1);
+            test.VolumeControl(efeitos, 1);
+        }
+        else
+        {
+            test.VolumeControl(musica, 0);
+            test.VolumeControl(efeitos, 0);
+        }
+    }
+    /// <summary>
+    /// Desliga a musica ou liga 
+    /// </summary>
+    public void OnOffMusic()
+    {
+        if (flag_OnOffMusic)
+        {
+            flag_OnOffMusic = false;
+            test.VolumeControl(musica, 0);
+        }
+        else
+        {
+            flag_OnOffMusic = true;
+            test.VolumeControl(musica, 1);
+        }
+    }
+    /// <summary>
+    /// Desliga o efeitos do jogo ou liga
+    /// </summary>
+    public void OnOffEffects()
+    {
+        if (flag_OnOffEffects)
+        {
+            flag_OnOffEffects = false;
+            test.VolumeControl(efeitos, 0);
+        }
+        else
+        {
+            flag_OnOffEffects = true;
+            test.VolumeControl(efeitos, 1);
+        }
+    }
+    /// <summary>
+    /// Desliga ou liga a musica e os efeitos do jogo
+    /// </summary>
+    public void OnOffMusicEffects()
+    {
+        if (flag_OnOffMusicEffects)
+        {
+            flag_OnOffMusicEffects = true;
+            test.VolumeControl(efeitos, 0);
+            test.VolumeControl(musica, 0);
+        }
+        else
+        {
+            flag_OnOffMusicEffects = false;
+            test.VolumeControl(efeitos, 1);
+            test.VolumeControl(musica, 1);
+        }
+    }
+    public void OnMusic()
+    {
+        flag_OnOffMusic = true;
+        test.VolumeControl(musica, 1);
+    }
+    public void OffMusic()
+    {
+        flag_OnOffMusic = false;
+        test.VolumeControl(musica, 0);
+    }
+    // END METODOS DA CLASSE 
+
+    void Start()
+    {
+        InitConf();
+        //Garante que o Valor inicial sempre vai ser o de SoundSystem caso as barras sejam declaradas.
+        if (volumeMusica != null)
+        {
+            test.init();// INICIALIZA A FUNÇÃO ATRIBUINDO OS VALORES GUARDADOS NO PLAYER PREF
+            volumeMusica.value = test.VolumeMusic;
+
+            if(volumeEfeitos != null)
+                volumeEfeitos.value = test.VolumeEffects;
+        }
+    }
+    void Update()
+    {
+        //Garante que só vai atualizar o volume de SoundSystem se a classe tiver atribuido as barras de volume.
+        if (volumeMusica != null && volumeEfeitos != null)
+        {
+            test.VolumeMusic = volumeMusica.value;
+            test.VolumeEffects = volumeEfeitos.value;
+
+            test.StoresNewVolume(volumeMusica.value, volumeEfeitos.value);
+
+            test.VolumeControl(musica, test.VolumeMusic);
+            test.VolumeControl(efeitos, test.VolumeEffects);
+    
+        }
+        //Caso não seja declarado as barras de volume ele apenas ganrante que o volume é o mesmo em qualquer canto.
+        else
+        {
+            test.VolumeControl(musica, test.VolumeMusic);
+            test.VolumeControl(efeitos, test.VolumeEffects);
+        }
+    }
+}
---
+++ MusicSystem/SoundSystem.cs
@@ -0,0 +1,83 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+/// <summary>
+/// Garante que tudo envolvendo controle de audio está interligado.
+/// </summary>
+public sealed class SoundSystem : MonoBehaviour
+{
+
+    // INIT ATRIBUTOS
+    private static SoundSystem _AudioConfiguration = new SoundSystem(); //Cria instanci dele mesmo.
+    public static SoundSystem Instance //Propriedade para instanciar a instancia.
+    {
+        get { return _AudioConfiguration; }
+    }
+    static float volumeMusic; //Volume da musica geral.
+    public float VolumeMusic
+    {
+        get { return volumeMusic; }
+        set { volumeMusic = value; }
+    }
+    static float volumeEffects; //Volume dos efeitos gerais.
+    public float VolumeEffects
+    {
+        get { return volumeEffects; }
+        set { volumeEffects = value; }
+    }
+    // END ATRIBUTOS
+
+    // INIT METODOS
+    /// <summary>
+    /// Pausa todas as musicas atribuidas a função.
+    /// </summary>
+    /// <param name="Audio"></param>
+    public void Pause(List<AudioSource> Audio)
+    {
+        for (int i = 0; i < Audio.Count; ++i)
+            Audio[i].Pause();
+    }
+    /// <summary>
+    /// Da play em todas as musicas atribuidas a função.
+    /// </summary>
+    /// <param name="Audio"></param>
+    public void Play(List<AudioSource> Audio)
+    {
+        for (int i = 0; i < Audio.Count; ++i)
+            Audio[i].Play();
+    }
+    /// <summary>
+    /// Garante a mudança de volume das musicas atribuidas a função.
+    /// </summary>
+    /// <param name="Audio"></param>
+    /// <param name="Volume"></param>
+    public void VolumeControl(List<AudioSource> Audio, float Volume)
+    {
+        for (int i = 0; i < Audio.Count; ++i)
+            Audio[i].volume = Volume;
+    }
+    /// <summary>
+    /// Armazena a configuração para a proxima vez que o usuario entrar no jogo.
+    /// </summary>
+    /// <param name="valueMusic"></param>
+    /// <param name="valueEffects"></param>
+    public void StoresNewVolume(float valueMusic, float valueEffects)
+    {
+        PlayerPrefs.SetFloat("Music", valueMusic);
+        PlayerPrefs.SetFloat("Effects", valueEffects);
+    }
+    //EMD METODOS
+    private SoundSystem() { }
+    public void init()
+    {
+        if (PlayerPrefs.HasKey("Music"))
+        {
+            VolumeMusic = PlayerPrefs.GetFloat("Music");
+            VolumeEffects = PlayerPrefs.GetFloat("Effects");
+        }
+        else
+        {
+            VolumeEffects = 1;
+            VolumeMusic = 1;
+        }
+    }
+}
